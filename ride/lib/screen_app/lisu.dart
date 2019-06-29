d2d27478b89febb3a32858a0abc63b0244819531
Bikash <sahbikash999@gmail.com>
2019-06-29 15:54:13 +0545

    Login And Sign Up Page Added

--- ride/lib/screen_app/lisu.dart
+++ ride/lib/screen_app/lisu.dart
@@ -6,12 +6,43 @@ class LogSign extends StatelessWidget {
     return MaterialApp(
       home: Material(
         color: Colors.white,
+        child: Container(
+          margin: EdgeInsets.only(top: 100.0),
         child: Column(
           children: <Widget>[
-            Image.asset('images/logo.png'),
+            Image.asset('images/logo.png',height:150.0,width:300.0,alignment:Alignment.center),
+            LoginButton(),
+            SignUpButton(),
           ],
         ),
+        ),
       ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        color: Colors.red,
+        child: Text("Log In"),
+        onPressed:null,
+      )
+    );
+  }
+}
+
+class SignUpButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: RaisedButton(
+          color: Colors.red,
+          child: Text("Sign Up"),
+          onPressed:null,
+        )
     );
   }
 }
